61f7f21e8607e2596ad0abefdf6f6e94d3d0526a
facugarote <85139558+facugarote@users.noreply.github.com>
2021-06-30 20:28:52 -0300

    Update app.js

--- proyecto_ejemplo_4.7/proyecto_ejemplo_4.7/app.js
+++ proyecto_ejemplo_4.7/proyecto_ejemplo_4.7/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.get('/mostrar_mensaje', (req,res) =>{
+app.get('/', (req,res) =>{
     res.sendFile(path.resolve(__dirname, './views/index.html'));  // Permite enviar un archivo HTML
 });
 
